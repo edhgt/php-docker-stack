c639e3854676442ffd1f17ef6640083d272d9f62
Danilo Hernandez <eldh.dev@gmail.com>
2025-06-19 17:01:04 -0600

    first commit

---
+++ nginx/scripts/setup-vhost.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Crear configuración para cada proyecto
+for PROJECT in /var/www/*; do
+  if [ -d "$PROJECT" ]; then
+    PROJECT_NAME=$(basename "$PROJECT")
+    cp /etc/nginx/templates/project.conf.template /etc/nginx/conf.d/${PROJECT_NAME}.conf
+    sed -i "s/{{PROJECT_NAME}}/${PROJECT_NAME}/g" /etc/nginx/conf.d/${PROJECT_NAME}.conf
+  fi
+done
+
