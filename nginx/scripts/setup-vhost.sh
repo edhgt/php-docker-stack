06ea45502bff388e33451f24b84b384879901e42
Danilo Hernandez <eldh.dev@gmail.com>
2025-07-23 19:38:32 -0600

    Updated

--- nginx/scripts/setup-vhost.sh
+++ nginx/scripts/setup-vhost.sh
@@ -4,7 +4,7 @@
 for PROJECT in /var/www/*; do
   if [ -d "$PROJECT" ]; then
     PROJECT_NAME=$(basename "$PROJECT")
-    cp /etc/nginx/templates/project.conf.template /etc/nginx/conf.d/${PROJECT_NAME}.conf
+    cp /etc/nginx/templates/project.conf.template.ssl /etc/nginx/conf.d/${PROJECT_NAME}.conf
     sed -i "s/{{PROJECT_NAME}}/${PROJECT_NAME}/g" /etc/nginx/conf.d/${PROJECT_NAME}.conf
   fi
 done
---
+++ node/docker-entrypoint.sh
@@ -0,0 +1,1 @@
+/bin/sh -c trap : TERM INT; sleep infinity & wait
